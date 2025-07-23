272e375bee86bebe1d96b303148cf7e9d074fea0
Sourav <sourav123singhgmaii.com@gmail.com>
2025-07-23 17:37:00 +0530

    Resolved Issue

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   host: true,
-  base: "/SimpleFoods",
   build: {
     outDir: 'dist',
   },
